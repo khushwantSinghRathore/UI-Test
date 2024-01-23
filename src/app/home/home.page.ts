d718949728fa53091f7c851590e6ab89427a20cc
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-01-23 21:12:06 +0530

    Api call via Http

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  baseurl: string = 'https://reqres.in/api/users?page=2';
+  apidata: any;
+  viewmode: boolean = true;
+  constructor() {
+    this.getdata();
+  }
 
-  constructor() {}
+  async getdata() {
+    const options = {
+      url: this.baseurl,
+      headers: {},
+      params: {},
+    };
 
+    const response: HttpResponse = await CapacitorHttp.get(options);
+    this.apidata = response.data.data;
+    console.log(this.apidata);
+
+    // or...
+    // const response = await CapacitorHttp.request({ ...options, method: 'GET' })
+
+    console.log('called', response);
+  }
 }
